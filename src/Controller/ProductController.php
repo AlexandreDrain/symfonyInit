d731128e672728d285d93cc2a4d9f895d13e7285
unknown <stagiaire@PC-B-8-34R9J32.b-doranco.prod>
2019-06-27 12:35:48 +0200

    Mise en place de la vue détail, liens de la navbar mis a jour, données des cards bien prise depuis la BDD

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,18 +2,43 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Produit;
 
 class ProductController extends AbstractController
 {
-    
     /**
-     * Affiche une page HTML (détails de produit)
+     * Affiche une page HTML (Liste des produits)
+     * @return Response
+     */
+    public function liste(): Response
+    {
+        // Récupération de .....
+        $repository = $this->getDoctrine()->getRepository(Produit::class);
+        //
+        $products = $repository->findAll();
+        //
+        return $this->render('products/liste.html.twig', ['products' => $products]);
+    }
+    /**
+     * Affiche une page HTML (détails du produit)
      * @return Response
      */
     public function show(string $slug): Response
     {
-        dump($slug);
-        return $this->render('products/show.html.twig');
+        $repository = $this->getDoctrine()->getRepository(Produit::class);
+        $product = $repository->findOneBy(['slug' => $slug]);
+        return $this->render('products/show.html.twig', ['product' => $product]);
+    }
+    /**
+     * Affiche une page HTML (Création d'un produit)
+     * @var  Request
+     * @return Response
+     */
+    public function create(Request $requestHTTP): Response
+    {
+        $this->addFlash('warning', 'Voulez-vous vraiment créer un porduit ?');
+        return $this->render('products/create.html.twig');
     }
 }
---
+++ src/Entity/Produit.php
@@ -0,0 +1,232 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProduitRepository")
+ */
+class Produit
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private $productName;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $productDecription;
+
+    /**
+     * @ORM\Column(type="decimal", precision=9, scale=2)
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $nbViews;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updateAt;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isPublished;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $imageName;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category", inversedBy="produits")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Tag", inversedBy="produits")
+     */
+    private $tags;
+
+    /**
+     * @ORM\Column(type="string", length=128, nullable=true, unique=true)
+     */
+    private $slug;
+
+    public function __construct()
+    {
+        $this->tags = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProductName(): ?string
+    {
+        return $this->productName;
+    }
+
+    public function setProductName(string $productName): self
+    {
+        $this->productName = $productName;
+
+        return $this;
+    }
+
+    public function getProductDecription(): ?string
+    {
+        return $this->productDecription;
+    }
+
+    public function setProductDecription(string $productDecription): self
+    {
+        $this->productDecription = $productDecription;
+
+        return $this;
+    }
+    
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getNbViews(): ?int
+    {
+        return $this->nbViews;
+    }
+
+    public function setNbViews(?int $nbViews): self
+    {
+        $this->nbViews = $nbViews;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeInterface
+    {
+        return $this->updateAt;
+    }
+
+    public function setUpdateAt(?\DateTimeInterface $updateAt): self
+    {
+        $this->updateAt = $updateAt;
+
+        return $this;
+    }
+
+    public function getIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageName(string $imageName): self
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    public function getCategory(): Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Tag[]
+     */
+    public function getTags(): Collection
+    {
+        return $this->tags;
+    }
+
+    public function addTag(Tag $tag): self
+    {
+        if (!$this->tags->contains($tag)) {
+            $this->tags[] = $tag;
+        }
+
+        return $this;
+    }
+
+    public function removeTag(Tag $tag): self
+    {
+        if ($this->tags->contains($tag)) {
+            $this->tags->removeElement($tag);
+        }
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
