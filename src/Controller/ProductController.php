74446bf15413f7f746f2769d72df47cd2924f078
unknown <stagiaire@PC-B-8-34R9J32.b-doranco.prod>
2019-06-28 17:04:31 +0200

    Mise en place des formValidator, insértion, update, delete des produits

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,11 +1,14 @@
 <?php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Entity\Produit;
 use App\Entity\Category;
+use App\Entity\Produit;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
@@ -23,6 +26,88 @@ class ProductController extends AbstractController
         return $this->render('products/liste.html.twig', ['products' => $products]);
     }
     /**
+     * Affiche une page HTML (Création d'un produit)
+     * @var  Request
+     * @return Response
+     */
+    public function create(Request $requestHTTP): Response
+    {
+        // Récupération du formulaire
+        $product = new Produit();
+        $formProduct = $this->createForm(ProductType::class, $product);
+        //
+        $formProduct->handleRequest($requestHTTP);
+        // On vérifie que le formulaire est sommis et valide
+        if ($formProduct->isSubmitted() && $formProduct->isValid()) {
+            // On sauvegarde le produit en BDD grace a un manager
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($product);
+            $manager->flush();
+
+            // Ajout d'un message flash
+            $this->addFlash('success', 'le produit a bien était ajouté');
+            // Petite redirection des familles
+            return $this->redirectToRoute('app_product_liste');
+        }
+
+        return $this->render(
+            'products/create.html.twig',
+            [
+            'formProduct' => $formProduct->createView()
+            ]
+        );
+    }
+
+    /**
+     * Affiche et traite le formulaire de modification d'un produit
+     * @param Request $requestHTTP
+     * @param Product $product
+     * @return Response
+     */
+    public function update(Request $requestHTTP, string $slug): Response
+    {
+        // Récupération du formulaire
+        $formProduct = $this->createForm(ProductType::class, $product);
+        // On envoie les données postées au formulaire
+        $formProduct->handleRequest($requestHTTP);
+        // On vérifie que le formulaire est sommis et valide
+        if ($formProduct->isSubmitted() && $formProduct->isValid()) {
+            // On sauvegarde le produit en BDD grace a un manager
+            $manager = $this->getDoctrine()->getManager();
+            $manager->flush();
+
+            // Ajout d'un message flash
+            $this->addFlash('warning', 'Le produit a bien était modifié');
+            // Petite redirection des familles
+            return $this->redirectToRoute('app_product_liste', [
+            'formProduct' => $formProduct->createView()
+            ]);
+        }
+
+        return $this->render(
+            'products/modify.html.twig',
+            [
+            'formProduct' => $formProduct->createView()
+            ]
+        );
+    }
+
+    /**
+     * Suppression d'un produit
+     * @param Produit $product
+     * @return Response
+     */
+    public function delete(Produit $product): Response
+    {
+        // On sauvegarde le produit en BDD grâce au manager
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($product);
+        $manager->flush();
+        // Ajout d'un message flash
+        $this->addFlash('danger', 'Le produit est supprimé');
+        return $this->redirectToRoute('app_product_liste');
+    }
+    /**
      * Affiche une page HTML (détails du produit)
      * @return Response
      */
@@ -34,28 +119,5 @@ class ProductController extends AbstractController
             throw $this->createNotFoundException('Produit innexistant');
         }
          return $this->render('products/show.html.twig', ['product' => $product]);
-    }
-    /**
-     * Affiche une page HTML (Création d'un produit)
-     * @var  Request
-     * @return Response
-     */
-    public function create(Request $requestHTTP): Response
-    {
-        // Récuperation d'une catégorie
-        $category = $this->getDoctrine()->getRepository(Category::class)->find(1);
-
-        // Création et remplissage du produit
-        $product = new Produit();
-        $product
-            ->setProductName('ventilateur')
-            ->setProductDescription('Pour faire de l\'air frais')
-            ->setImageName('ventile.jpg')
-            ->setIsPublished(true)
-            ->setPrice(59.99)
-            ->setCategory($category);
-        ;
-        dd($product);
-        return $this->render('products/create.html.twig');
     }
 }
--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Cocur\Slugify\Slugify;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProduitRepository")
@@ -81,6 +82,20 @@ class Produit
         $this->createdAt = new \DateTime();
     }
 
+    /**
+     * Met a jour le slug par rapport au name
+     * @return Produit
+     */
+    public function updateSlug(): self
+    {
+        // On récupère le slugger
+        $slugify = new Slugify();
+        // mise a jour du slug par rapport au name
+        $this->slug = $slugify->slugify($this->productName);
+        // Pour le chainage
+        return $this;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -94,18 +109,19 @@ class Produit
     public function setProductName(string $productName): self
     {
         $this->productName = $productName;
+        $this->updateSlug();
 
         return $this;
     }
 
     public function getProductDescription(): ?string
     {
-        return $this->productDecription;
+        return $this->productDescription;
     }
 
-    public function setProductDescription(string $productDecription): self
+    public function setProductDescription(string $productDescription): self
     {
-        $this->productDecription = $productDecription;
+        $this->productDescription = $productDescription;
 
         return $this;
     }
@@ -182,7 +198,7 @@ class Produit
         return $this;
     }
 
-    public function getCategory(): Category
+    public function getCategory(): ?Category
     {
         return $this->category;
     }
